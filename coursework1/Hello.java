2158b99087f3eb8c59c622f9349ffc58caac2a03
Balaaj Naeem <ko029033@student.reading.ac.uk>
2025-11-09 15:00:12 +0000

    Move files into coursework1 folder

---
+++ coursework1/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    private static final String STUDENT_NUMBER = "34029033";
+
+    public static void main(String[] args) {
+        System.out.println("Hello, student " + STUDENT_NUMBER + ".");
+    }
+}
